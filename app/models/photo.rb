a6a9164dc3e8eb06b476d95cafcb8473e462d2f5
rubinmd <matt@Matthews-MBP.attlocal.net>
2015-11-20 11:33:28 -0600

    housekeeping
    Cleaned up new and edit photos. Made photo URL required. Made the likes
    show in an order that makes sense.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,5 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :fans, :through => :likes, :source => :user
   validates :user_id, :presence=>true
+  validates :image, :presence=>true
 end
